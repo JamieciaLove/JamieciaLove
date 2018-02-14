71329b28b92e3c617ab0e75bd33020aef0491f48
JamieciaLove <jlove777@outlook.com>
2018-02-14 15:14:39 -0500

    CYOA script

---
+++ projects/cyoa.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+Choose your fate: Red Pill or Blue Pill?
+Blue pill - Welp you will continue living in blissful ignorance on your ride to the gallows. Enjoy.
+Red Pill - Are you sure you made the right choice? If not, too late. I will ask you a series of 5 questions. What is the prize after making it to the end? If you make it to the end you will get completely filled with the satisfaction of knowing you made it to the end. Are you ready to play and discover the harsh realities of life?
+No – Well, off to the gallows. Try again.
+Yes – What is the only mammal that can’t jump? Elephants or Giraffes?
+Giraffes – Off to the gallows. Try again.
+Elephants – Well looks like you have the memory of an elephant. Who invented the electric chair? Carpenter or Dentist?
+Carpenter - Today seems like a beautiful, sunny day to send you to the gallows. Try again.
+Dentist- I bet you’ll think twice before sitting in that chair again huh? What is the most commonly spoken language in the world?
+English- Smh. Believe it or not the world doesn’t revolve around English. Off to the bloody gallows you go! Try again.
+Chinese- Nǐ hǎo. No need to dig a hole to China. I am sure someone in your neighborhood speaks Chinese. What is the most common name in the world? Muhammed or John?
+John- Don’t blame John for your downfall! Off to the gallows. Try again.
+Muhammed – YAYYYYY!!! You’re completely filled with the satisfaction of making it to the end. Now, you get one &phone; call before going to the gallows.
