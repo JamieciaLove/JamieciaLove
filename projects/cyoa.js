755c02c9b448756f7f17b4a5ac749216adb5f3bd
JamieciaLove <jlove777@outlook.com>
2018-02-24 11:08:39 -0500

    latest changes

--- projects/cyoa.js
+++ projects/cyoa.js
@@ -1,6 +1,41 @@
 
 
-// Question prompts
+var qResponses = false; 
+var questions = ["Choose your fate: Red Pill or Purple Pill?",
+"Are you sure you made the right choice? If not, too late. I will ask you a series of 5 questions. What is the prize after making it to the end? If you make it to the end you will get completely filled with the satisfaction of knowing you made it to the end. Are you ready to play and discover the harsh realities of life?",
+"What is the only mammal that can’t jump? Elephants or Giraffes?",
+"Well looks like you have the memory of an elephant. Who invented the electric chair? Carpenter or Dentist?",
+"I bet you’ll think twice before sitting in that chair again huh? What is the most commonly spoken language in the world?",
+"Nǐ hǎo. No need to dig a hole to China. I am sure someone in your neighborhood speaks Chinese. What is the most common name in the world? Muhammed or John?" ]
+
+var responses = ["Well I don't like the color red. Off to the gallows. Try again", 
+"Well, off to the gallows. Try again.",
+"Off to the gallows you go. Try again.",
+"Today seems like a beautiful, sunny day to send you to the gallows. Try again.",
+"Smh. Believe it or not the world doesn’t revolve around English. Off to the bloody gallows you go! Try again.",
+"Don’t blame John for your downfall! Off to the gallows. Try again.", "YAYYYYY!!! You’re completely filled with the satisfaction of making it to the end. Now, you get one &phone; call before going to the gallows."]
+
+
+alert("Welcome to the game Red Pill/Purple Pill. The pill you choose will determine your fate.");
+var response = prompt(questions[0]);
+if (response.toUpperCase() === "PURPLE") {
+  qResponses === true;
+}
+ else {
+   (responses[0]); qResponses == false; 
+ }
+
+ var response = prompt(questions[1]);
+
+ if (qResponses === true) {
+  prompt("lovely")
+}
+
+
+
+
+
+/*Question prompts first draft 
 var questionPill = false;
 alert("Welcome to the game Red Pill/Purple Pill. The pill you choose will determine your fate.");
 var response = prompt("Choose your fate: Red Pill or Purple Pill? (Type only color)");
@@ -18,11 +53,7 @@ if (response.toUpperCase() === "PURPLE") {
 
 if (questionPill === true) {
   prompt(`Are you sure you made the right choice? If not, too late. I will ask you a series of 5 questions. What is the prize after making it to the end? If you make it to the end you will get completely filled with the satisfaction of knowing you made it to the end. Are you ready to play and discover the harsh realities of life?`);
-}
-
-
-
-
+}*/
 
 
 
@@ -31,7 +62,7 @@ if (questionPill === true) {
 // Red Pill - Are you sure you made the right choice? If not, too late. I will ask you a series of 5 questions. What is the prize after making it to the end? If you make it to the end you will get completely filled with the satisfaction of knowing you made it to the end. Are you ready to play and discover the harsh realities of life?
 // No – Well, off to the gallows. Try again.
 // Yes – What is the only mammal that can’t jump? Elephants or Giraffes?
-// Giraffes – Off to the gallows. Try again.
+// Giraffes – Off to the gallows you go. Try again.
 // Elephants – Well looks like you have the memory of an elephant. Who invented the electric chair? Carpenter or Dentist?
 // Carpenter - Today seems like a beautiful, sunny day to send you to the gallows. Try again.
 // Dentist- I bet you’ll think twice before sitting in that chair again huh? What is the most commonly spoken language in the world?
